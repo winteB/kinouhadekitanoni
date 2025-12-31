67ca70fbe1215be0b1b3ab9d2257c1121ce46b8b
winteB <110764252+winteB@users.noreply.github.com>
2025-12-31 17:32:10 +0900

    Add files via upload
    Signed-off-by: winteB <110764252+winteB@users.noreply.github.com>

--- src/main/java/command/Member.java
+++ src/main/java/command/Member.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import command.member.MemberLogin;
+import command.member.MemberSave;
+import common.CommonExecute;
+
 /**
  * Servlet implementation class Member
  */
@@ -42,26 +46,36 @@ public class Member extends HttpServlet {
       }
       
       //여기서부터 페이지로 보내는 용도
+      //로그인
       if(gubun.equals("login")) {
 
          view = "member/member_login.jsp";
-      }
-      else if(gubun.equals("password")) {
+      }//로그인 시도
+      else if(gubun.equals("memberLogin")) {
+    	  CommonExecute mem = new MemberLogin();
+    	  mem.execute(request);
+          view = "common_alert.jsp";
+       }else if(gubun.equals("password")) {
 
          view = "member/member_password.jsp";
       }else if(gubun.equals("join")) {
-
+    	  
          view = "member/member_join.jsp";
+      
+      //회원등록
+      }else if(gubun.equals("save")){
+    	  CommonExecute member = new MemberSave();
+    	  member.execute(request);
+ 			
+    	  view="common_alert.jsp";
+    
       }else if(gubun.equals("myinfo")) {
 
          view = "member/member_myinfo.jsp";
       }else if(gubun.equals("myyoyaku")) {
 
-          view = "member/member_myyoyaku_list.jsp";
-       }else if(gubun.equals("myyoyakuView")) {
-
-           view = "member/member_myyoyaku_view.jsp";
-        }
+         view = "member/member_myyoyaku.jsp";
+       }
       
       RequestDispatcher rd = request.getRequestDispatcher(view);
       rd.forward(request, response);
