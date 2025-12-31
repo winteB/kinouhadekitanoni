b4b39f2ac4ae12e378216b61569819d881aa981b
smcoe <smcoe@DESKTOP-68QE9AB>
2025-12-31 14:11:41 +0900

    1230 작업

---
+++ src/main/java/command/Paymant.java
@@ -0,0 +1,72 @@
+package command;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Paymant
+ */
+@WebServlet("/Paymant")
+public class Paymant extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Paymant() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+
+		String view = "";
+		String gubun = request.getParameter("t_gubun");
+		//gubun 값으로 낚시 예약페이지에서 온 건지 캠핑 예약 페이지에서 온 건지 구분
+		//gubun 값 null/공백 처리
+		if(gubun == null) {
+			gubun = "";
+		}
+		if(gubun.equals("")) {
+			gubun = "main";
+		}
+		
+		//여기서부터 페이지로 보내는 용도
+		//잘못 넘어온 거면 커버 페이지로 보냄
+		if(gubun.equals("main")) {
+
+			view = "cover.jsp";
+		}
+		else if(gubun.equals("camping")) {
+			//성공, 실패 검증 클래스
+			view = "common_alert.jsp";
+		}
+		else if(gubun.equals("fishing")) {
+			//성공, 실패 검증 클래스
+			view = "common_alert.jsp";
+		}
+		
+		
+		RequestDispatcher rd = request.getRequestDispatcher(view);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
